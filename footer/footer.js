ed0a48bcc6ddbc3ea5457a780fa4a6aa25e58224
Raul Barreto <raul_barreto99@live.com>
2024-10-14 20:14:09 -0300

    commit
    initial

---
+++ footer/footer.js
@@ -0,0 +1,46 @@
+export const varFoot = `<section class="firstFooter">
+            <div class="divFooter">
+                <div class="divMore">
+                    <h2>Sobre o Projeto</h2>
+                    <p>A ideia é ter um portal onde você pode obter todas as informações que precisar sobre Call Of
+                        Duty, desde o multiplayer, zombies, easter eggs e etc. Obrigado!</p>
+                </div>
+                <div class="divSocial">
+                    <h2>Redes Sociais</h2>
+                    <div class="divAlignSocial">
+                        <img src="https://th.bing.com/th/id/OIP.jn2uStuj4NBZgs8zFNJKmAHaHa?rs=1&pid=ImgDetMain" alt="">
+                        <span>zNexus_v_Souls</span>
+                    </div>
+        
+                    <div class="divAlignSocial">
+                        <img src="https://th.bing.com/th/id/OIP.hRp0b3sxRZQoEL_38D6qiwHaHa?rs=1&pid=ImgDetMain" alt="">
+                        <span>zNexus</span>
+                    </div>
+        
+                    <div class="divAlignSocial">
+                        <img src="https://th.bing.com/th/id/OIP.oNsGQK4fi7_bL9sLmGg1TQHaHa?rs=1&pid=ImgDetMain" alt="">
+                        <span>zNexus v Souls</span>
+                    </div>
+                </div>
+                <div class="divContact">
+                    <h2>Informaçoes de Contato</h2>
+        
+                    <div class="divAlignSocial">
+                        <img src="https://cdn.pixabay.com/photo/2017/09/12/06/26/home-2741413_960_720.png" alt="">
+                        <span>Raul Barreto</span>
+                    </div>
+        
+                    <div class="divAlignSocial">
+                        <img src="https://th.bing.com/th/id/R.1d3e6b4f55eb2306e53081130d6495ad?rik=wZe80nk1nomn5Q&pid=ImgRaw&r=0"
+                            alt="">
+                        <span>znexusvsouls</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="secondFooter">
+            <div>
+                <span>All rights reserved | Raul Barreto</span>
+                <span>Privacy Police | Term & Conditions</span>
+            </div>
+        </section>`
---
+++ main/script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementById('menu');
+menu.addEventListener("click", function () {
+
+    let nav = document.getElementById('nav')
+
+    if (nav.style.display == "none") {
+        nav.style.display = "flex";
+        menu.style.backgroundImage = "url(https://th.bing.com/th/id/OIP.w8f8sMaOkTINNDq0W0epEQHaHa?w=512&h=512&rs=1&pid=ImgDetMain)"
+    } else {
+        nav.style.display = "none";
+        menu.style.backgroundImage = "url(https://static.vecteezy.com/system/resources/previews/000/440/847/original/menu-vector-icon.jpg)"
+    }
+});
+
+document.getElementById('treyarch').addEventListener("click", function () {
+
+    window.location.href = `treyarchindex.html`;
+});
+
+document.getElementById('bo2').addEventListener("click", function () {
+
+    window.location.href = `../bo2main/bo2main.html`;
+});
+
